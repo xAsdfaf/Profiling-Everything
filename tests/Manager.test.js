2c791e7d002cdf6baeb816990fe9644f8224092e
xAsdfaf <georgejordanjr8@gmail.com>
2023-03-01 12:39:25 -0600

    completed manager test script.

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,1 +1,13 @@
 const Manager = require('../lib/Manager');
+
+test('creates a Manager object', () => {
+    const manager = new Manager('Hunter', 22, 'hoskarson@gmail.com');
+
+    expect(manager.numberOffice).toEqual(expect.any(String));
+});
+
+test('creates a Manager object', () => {
+    const manager = new Manager('Hunter', 22, 'hoskarson@gmail.com');
+
+    expect(manager.numberOffice).toEqual(expect.any(String));
+});
