e35162068e05eba135ef84d190293d0a125768b4
xAsdfaf <georgejordanjr8@gmail.com>
2023-03-01 12:24:39 -0600

    added engineer tests and constructors.

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,1 +1,22 @@
 const Engineer = require('../lib/Engineer');
+
+// creating engineer object
+test('creates an Engineer object', () => {
+    const employee = new Employee('Hunter', 22, 'hoskarson@gmail.com');
+
+    expect(engineer.github) .toEqual(expect.any(String));
+});
+
+// gets github from getGithub()
+test('gets engineer github value', () => {
+    const employee = new Employee('Hunter', 22, 'hoskarson@gmail.com');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+// gets role from getRole()
+test('gets role of employee', () => {
+    const employee = new Employee('Hunter', 22, 'hoskarson@gmail.com');
+
+    expect(engineer.getRole()).toEqual("Engineer");
+});
